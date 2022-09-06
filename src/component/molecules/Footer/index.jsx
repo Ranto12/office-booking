adce9e25b0f224d69b0799005bd81fa92b1c1773
Ranto <rantoirwanda08@gmail.com>
2022-09-06 11:15:21 +0700

    responsive footer

--- src/component/molecules/Footer/index.jsx
+++ src/component/molecules/Footer/index.jsx
@@ -3,7 +3,7 @@ import { Facebook, Instagram, Linkedin, Logo } from '../../../assets'
 
 const index = () => {
   return (
-    <div className='mx-32 h-auto my-5   text-center py-5'>
+    <div className='mx-32 h-auto my-5   text-center py-5 '>
         <div className='w-full md:flex px-5'>
             <div className='md:w-2/3 justify-start'>
                 <img src={Logo} alt="logo" className='mx-auto md:ml-5'/>
@@ -21,59 +21,30 @@ const index = () => {
                     <img src={Linkedin} alt="Linkedin" />
                 </div>
             </div>
-            <div className='md:w-1/3 w-full md:flex md:text-left'>
-                <div className='text-base font-medium gap-y-20 w-full md:w-1/2 ' >
-                    <p className='font-semibold text-2xl  mb-5'>SewaKantor</p>
-                    <p>Home</p>
-                    <p>Kantor</p>
-                    <p>Team</p>
+            <div className='md:w-1/3 w-full md:flex md:text-left   '>
+                <div className='text-base font-medium gap-y-20 w-full md:w-1/2 right-0 md:flex justify-end' >
+                    <div className='md:max-w-max '>
+                        <p className='font-semibold text-2xl  mb-5'>SewaKantor</p>
+                        <p>Home</p>
+                        <p>Kantor</p>
+                        <p>Team</p>
+                    </div>
                 </div>
-                <div className='text-base my-5 md:mt-0 font-medium gap-y-20 w-full md:w-1/2 ' >
-                    <p className='font-semibold text-2xl  mb-5'>Bantuan</p>
-                    <p>Syarat & Ketentuan</p>
-                    <p>Kebijakan Privasi</p>
-                    <p>About Us</p>
-                    <p>FAQ</p>
+                <div className='text-base my-5 md:mt-0 font-medium gap-y-20 w-full md:w-1/2  md:flex justify-end' >
+                    <div className='md:max-w-max '>
+                        <p className='font-semibold text-2xl  mb-5'>Bantuan</p>
+                        <p>Syarat & Ketentuan</p>
+                        <p>Kebijakan Privasi</p>
+                        <p>About Us</p>
+                        <p>FAQ</p>
+                    </div>
                 </div>
             </div>
         </div>
         <hr  className='border-b-1 border-black '/>
         <p className='font-semibold text-lg'>All rights reserved © SewaKantor Corporation 2021</p>
     </div>
-    // <div className='mx-32 h-auto my-5  bg-white text-center py-5'>
-    //     <div className='w-full  px-5'>
-    //         <div>
-    //             <img src={Logo} alt="logo" className='mx-auto'/>
-    //        <div className='block'>
-    //         <p className='block text-lg font-medium'>Alterra Academy
-    //                 <br />
-    //             Jalan Pemuda No. 9 Rawamangun,
-    //                 <br />
-    //             Jakarta Timur - 13220</p>
-    //         </div>
-    //         <div className='flex gap-3 justify-center my-4 mb-10'>
-    //             <img src={Facebook} alt="facobook" />
-    //             <img src={Instagram} alt="Instagram" />
-    //             <img src={Linkedin} alt="Linkedin" />
-    //         </div>
-    //         </div>
-    //         <div className='text-base font-medium gap-y-20' >
-    //             <p className='font-semibold text-2xl  mb-5'>SewaKantor</p>
-    //             <p>Home</p>
-    //             <p>Kantor</p>
-    //             <p>Team</p>
-    //         </div>
-    //         <div className='text-base mt-5 font-medium gap-y-20' >
-    //             <p className='font-semibold text-2xl  mb-5'>Bantuan</p>
-    //             <p>Syarat & Ketentuan</p>
-    //             <p>Kebijakan Privasi</p>
-    //             <p>About Us</p>
-    //             <p>FAQ</p>
-    //         </div>
-    //     </div>
-    //     <hr  className='border-b-1 border-black '/>
-    //     <p className='font-semibold text-lg'>All rights reserved © SewaKantor Corporation 2021</p>
-    // </div>
+    
   )
 }
 
